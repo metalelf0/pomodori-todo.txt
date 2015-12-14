76bf310fd3caf341f77ac0020472836faef76504
Andrea Schiavini <andrea.schiavini@gmail.com>
2015-12-14 09:55:48 +0100

    Adding experimental support for iterm2 window title

--- lib/countdown.rb
+++ lib/countdown.rb
@@ -4,20 +4,26 @@ class Countdown
     seconds.downto(0) do |current_seconds|
       sleep 1
       write_tmux(current_seconds) if (options[:services] || []).include?(:tmux)
+      set_window_title(current_seconds) if (options[:services] || []).include?(:iterm2)
       STDOUT.write "[RUNNING] #{to_minutes(current_seconds)}\r"
     end
   end
 
   private
 
-  def write_tmux current_seconds
+  def write_tmux(current_seconds)
     `echo #{to_minutes(current_seconds, :tmux)} > ~/.pomo.txt.tmux` 
   end
 
+  def set_window_title(current_seconds)
+    title = to_minutes(current_seconds, :tmux)
+    `echo -ne "\e]1;#{title}\a"`
+  end
+
   def to_minutes seconds, format=:standard
-  	minutes = seconds / 60
-  	mins    = sprintf("%02d", minutes)
-  	secs    = sprintf("%02d", seconds % 60)
+    minutes = seconds / 60
+    mins    = sprintf("%02d", minutes)
+    secs    = sprintf("%02d", seconds % 60)
     if format == :standard
       "#{mins}:#{secs} [#{dots_for(minutes)}]"
     else
@@ -25,8 +31,9 @@ class Countdown
     end
   end
 
+
   def dots_for mins
-  	"#{'.' * (25 - mins)}#{' ' * mins}"
+    "#{'.' * (25 - mins)}#{' ' * mins}"
   end
 
 end
