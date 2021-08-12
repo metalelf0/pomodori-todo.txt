65d2c50087bd379a723e814e56837eeda8cc6999
Coyne, Charles <charles.coyne@bhp.com>
2021-08-12 23:12:18 +1000

    Fix date format in file_logger.rb

--- lib/file_logger.rb
+++ lib/file_logger.rb
@@ -11,11 +11,11 @@ class FileLogger
 
   def notify_start task
     add_separator_if_new_day
-    File.open(pomodoro_log_file, "a") { |file| file.puts "#{Time.now.strftime('%Y/%m/%d %H:%M')} Pomodoro nr. #{sprintf("% 2d", task.pomodori + 1)} started:   #{task.text}" }
+    File.open(pomodoro_log_file, "a") { |file| file.puts "#{Time.now.strftime('%Y-%m-%d %H:%M')} Pomodoro nr. #{sprintf("% 2d", task.pomodori + 1)} started:   #{task.text}" }
   end
 
   def notify_completed task
-    File.open(pomodoro_log_file, "a") { |file| file.puts "#{Time.now.strftime('%Y/%m/%d %H:%M')} Pomodoro nr. #{sprintf("% 2d", task.pomodori + 1)} completed: #{task.text}" }
+    File.open(pomodoro_log_file, "a") { |file| file.puts "#{Time.now.strftime('%Y-%m-%d %H:%M')} Pomodoro nr. #{sprintf("% 2d", task.pomodori + 1)} completed: #{task.text}" }
   end
 
   private
@@ -23,7 +23,7 @@ class FileLogger
   def add_separator_if_new_day
     return if File.zero?(pomodoro_log_file)
     last_day = File.open(pomodoro_log_file, "r")  { |file| file.readlines.last.split(" ")[0]}
-    today    = Time.now.strftime('%Y/%m/%d')
+    today    = Time.now.strftime('%Y-%m-%d')
     File.open(pomodoro_log_file, "a") { |file| file.puts "\n-----------\n\n" } if last_day != today
   end
 end
